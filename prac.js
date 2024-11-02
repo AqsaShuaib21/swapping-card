0f60445993450a3e37341d1decd9a0ac7eca4db8
Aqsa shuaib <aqsashuaib077@gmail.com>
2024-11-02 14:49:49 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,12 @@
+let marks=prompt("enter marks");
+if(marks>=80&& marks <=100){
+    console.log(marks ,"A");
+}else if(marks>=70&& marks <=79){
+    console.log(marks ,"B");
+}else if(marks>=60&& marks <=69){
+    console.log(marks ,"C");
+}else if(marks>=50&& marks <=59){
+    console.log(marks ,"D");
+}else{
+    console.log("faill!!!");
+}
